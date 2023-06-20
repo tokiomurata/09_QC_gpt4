a4c5a57cb4405305d612aae53f1e2dfadec9291c
tokiomurata <tokio.murata@toshiba.co.jp>
2023-06-20 15:20:47 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo"
+        model="gpt-3.5-turbo",
 #        model="gpt-4-0613",
         messages=messages
     )
