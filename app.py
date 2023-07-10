2669d2f8784ab5aac00f04a671b93691159f0b5c
tokiomurata <tokio.murata@toshiba.co.jp>
2023-07-10 10:05:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def communicate():
 
     response = openai.ChatCompletion.create(
 #        model="gpt-3.5-turbo",
-        model="gpt-4-0613",
+        model="gpt-4",
         messages=messages
     )
 
